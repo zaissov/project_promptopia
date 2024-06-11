a481de914b084484564646d44d94ba8b90cc098b
zaissov <zaissov@yahoo.com>
2024-06-11 11:54:03 +0100

    last commit

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+   
+   
+     
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+          port: '',
+          pathname: '/**/**',
+        },
+      ],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
+  
+  module.exports = nextConfig
